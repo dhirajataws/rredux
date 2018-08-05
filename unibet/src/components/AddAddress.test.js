fb5b2cc37e447e71bd10813f524d2e51b13ccfe6
dhirajataws <dhirajataws@gmail.com>
2018-08-06 09:20:14 +1000

    next

--- unibet/src/components/AddAddress.test.js
+++ unibet/src/components/AddAddress.test.js
@@ -4,7 +4,6 @@ import {configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()})
-
 it('AddAddress renders ', () => {
   const wrapper = shallow(<AddAddress tempAddress={{firstName: '', lastName: '', phoneNo: '', seqNo: ''}}/>)
   expect(wrapper.find('form').exists()).toBe(true)
